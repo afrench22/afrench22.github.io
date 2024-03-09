179ae7b744fd1b966afa5eb9f6098ebaa25e63cb
afrench22 <161403379+afrench22@users.noreply.github.com>
2024-03-09 16:58:58 -0600

    Update SDS-IAT.js

--- SDS-IAT.js
+++ SDS-IAT.js
@@ -6,39 +6,39 @@ define(["pipAPI", "https://afrench22.github.io/quiat9.js"], function (
 
   return iatExtension({
     category1: {
-      name: "Black people", //Will appear in the data.
+      name: "SEXUALLY ACTIVE", //Will appear in the data.
       title: {
-        media: { word: "Black people" }, //Name of the category presented in the task.
+        media: { word: "SEXUALLY ACTIVE" }, //Name of the category presented in the task.
         css: { color: "#31940F", "font-size": "2em" }, //Style of the category title.
         height: 4, //Used to position the "Or" in the combined block.
       },
       stimulusMedia: [
         //Stimuli content as PIP's media objects
-        { word: "Ashley" },
-        { image: "black2.jpg" },
-        { image: "black3.jpg" },
-        { image: "black4.jpg" },
-        { image: "black5.jpg" },
-        { image: "black6.jpg" },
+        { word: "male" },
+        { word: "male" },
+        { word: "male" },
+        { word: "male" },
+        { word: "male" },
+        { word: "male" },
       ],
       //Stimulus css (style)
       stimulusCss: { color: "#31940F", "font-size": "1.8em" },
     },
     category2: {
-      name: "White people", //Will appear in the data.
+      name: "Sexually Passive", //Will appear in the data.
       title: {
-        media: { word: "White people" }, //Name of the category presented in the task.
+        media: { word: "Sexually Passive" }, //Name of the category presented in the task.
         css: { color: "#31940F", "font-size": "2em" }, //Style of the category title.
         height: 4, //Used to position the "Or" in the combined block.
       },
       stimulusMedia: [
         //Stimuli content as PIP's media objects
-        { image: "yf1.jpg" },
-        { image: "yf4.jpg" },
-        { image: "yf5.jpg" },
-        { image: "ym2.jpg" },
-        { image: "ym3.jpg" },
-        { image: "ym5.jpg" },
+        { word: "female" },
+        { word: "female" },
+        { word: "female" },
+        { word: "female" },
+        { word: "female" },
+        { word: "female" },
       ],
       //Stimulus css
       stimulusCss: { color: "#31940F", "font-size": "1.8em" },
