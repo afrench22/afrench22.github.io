965d1649d47e2da22743b432190392ecd1d34e0e
Brent McKee <brent.mckee@siden.io>
2024-03-08 19:59:29 -0800

    add local copy of quiat9.js

--- SDS-IAT.js
+++ SDS-IAT.js
@@ -1,5 +1,5 @@
 
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://afrench22.github.io/quiat9.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
