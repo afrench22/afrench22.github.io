10dfa48ecd22420ce1e4cbfcd925f12192db9049
afrench22 <161403379+afrench22@users.noreply.github.com>
2024-03-11 14:18:38 -0500

    Update quiat9.js

--- quiat9.js
+++ quiat9.js
@@ -36,8 +36,8 @@ define(["pipAPI", "pipScorer", "underscore"], function (
 			category1: {
 				name : 'Male', //Will appear in the data and in the default feedback message.
 				title : {
-					media : {word : 'Male' }, //Name of the category presented in the task.
-					css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+					media : { word : 'Male' }, //Name of the category presented in the task.
+					css : { color:'#31940F','font-size':'2em'}, //Style of the category title.
 					height : 4 //Used to position the "Or" in the combined block.
 				},
 				stimulusMedia : [ //Stimuli content as PIP's media objects
