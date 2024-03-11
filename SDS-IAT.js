a309be660b12056745b3fce89d61a77999a954ca
afrench22 <161403379+afrench22@users.noreply.github.com>
2024-03-10 20:24:32 -0500

    Update SDS-IAT.js

--- SDS-IAT.js
+++ SDS-IAT.js
@@ -39,39 +39,6 @@ define(["pipAPI", "https://afrench22.github.io/quiat9.js"], function (
       //Stimulus css
       stimulusCss: { color: "#31940F", "font-size": "1.8em" },
     },
-    attribute2: {
-         name: "Sexually Passive", //Will appear in the data and in the default feedback message.
-         title: {
-          media: { word: "Sexually Passive" }, //Name of the category presented in the task.
-          css: { color: "#0000FF", "font-size": "2.3em" }, //Style of the category title.
-          height: 4, //Used to position the "Or" in the combined block.
-        },
-        stimulusMedia: [
-          //Stimuli content as PIP's media objects
-          { word: "Prude" },
-          { word: "Innocent" },
-          { word: "Modest" },
-          { word: "Naive" }
-        ],
-        //Stimulus css (style)
-        stimulusCss: { color: "#0000FF", "font-size": "2.3em" },
-	},		
-	attribute1: {
-         name: "Sexually Active", //Will appear in the data and in the default feedback message.
-         title: {
-          media: { word: "Sexually Active" }, //Name of the category presented in the task.
-          css: { color: "#0000FF", "font-size": "2.3em" }, //Style of the category title.
-          height: 4, //Used to position the "Or" in the combined block.
-        },
-        stimulusMedia: [
-          //Stimuli content as PIP's media objects
-          { word: "Dominant" },
-          { word: "Seducer" },
-          { word: "Vulgar" },
-          { word: "Explicit" }
-        //Stimulus css (style)
-        stimulusCss: { color: "#336600", "font-size": "2.3em" }
-      },
     base_url: {
       //Where are your images at?
       image: "https://afrench22.github.io/images/",
